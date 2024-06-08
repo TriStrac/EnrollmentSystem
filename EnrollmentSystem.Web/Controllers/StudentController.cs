b47c643cb6eec220be44e9514e0878d34ae99014
TriStrac <borloloy777leigh@gmail.com>
2024-06-09 03:07:37 +0800

    Added view pages

---
+++ EnrollmentSystem.Web/Controllers/StudentController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EnrollmentSystem.Web.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult StudentEntry()
+        {
+            return View();
+        }
+
+        public IActionResult SubjectEntry()
+        {
+            return View();
+        }
+
+        public IActionResult ScheduleEntry()
+        {
+            return View();
+        }
+        
+        public IActionResult EnrollStudentToSubject()
+        {
+            return View();
+        }
+    }
+}
