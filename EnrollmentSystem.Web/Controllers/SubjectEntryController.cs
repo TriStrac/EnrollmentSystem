a0f1a98ef19d951aadf391eea96a0ec2cacf950f
TriStrac <borloloy777leigh@gmail.com>
2024-06-09 07:23:47 +0800

    subject entry done

--- EnrollmentSystem.Web/Controllers/SubjectEntryController.cs
+++ EnrollmentSystem.Web/Controllers/SubjectEntryController.cs
@@ -1,12 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EnrollmentSystem.Web.Data;
+using EnrollmentSystem.Web.Models.Database;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace EnrollmentSystem.Web.Controllers
 {
     public class SubjectEntryController : Controller
     {
+        private readonly EnrollmentSystemDbContext _context;
+
+        public SubjectEntryController(EnrollmentSystemDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
         public IActionResult SubjectEntry()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SubmitSubject(Subject subject)
+        {
+            // Check for duplicate SubjectCode
+            if (await _context.SubjectProperty.AnyAsync(s => s.SubjectCode == subject.SubjectCode))
+            {
+                ModelState.AddModelError("SubjectCode", "Duplicate subject code");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.SubjectProperty.Add(subject);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("SubjectEntry");
+            }
+
+            return View("SubjectEntry", subject);
         }
     }
 }
